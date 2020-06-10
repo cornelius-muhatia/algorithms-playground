fe315f2372018943498241ab2e473cbfda988a70
Cornelius Muhatia <muhatiacornelius@gmail.com>
2020-06-10 23:35:49 +0300

    Done Bellman ford algorithm

---
+++ src/main/java/com/cmuhatia/playground/graph/BellmanFord.java
@@ -0,0 +1,82 @@
+/*
+ * Copyright 2019 Cornelius M.
+ * <p>
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * <p>
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * <p>
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.cmuhatia.playground.graph;
+
+import java.util.LinkedList;
+import java.util.Map;
+
+/**
+ * @author Cornelius M.
+ * @version 1.0.0, 09/06/2020
+ */
+public class BellmanFord {
+
+    /**
+     * Relaxes nodes of a graph
+     *
+     * @param graph {@link WeightedGraph}
+     * @param <T>  Node generic reference
+     * @return true if one or more nodes were relaxed and false otherwise
+     */
+    private static <T> boolean relaxGraph(WeightedGraph<T> graph){
+        boolean relaxed = false;
+        for (WeightedGraph.Node<T> node : graph) {
+            if(node.getWeight() != null){
+                for (Map.Entry<WeightedGraph.Node<T>, Double> entry : node.getNeighbours().entrySet()) {
+                    WeightedGraph.Node<T> key = entry.getKey();
+                    Double value = entry.getValue();
+                    if(key.getWeight() == null){
+                        key.setWeight(node.getWeight() + value);
+                        key.setParent(node);
+                        relaxed = true;
+                    } else if(key.getWeight() > (node.getWeight() + value)){
+                        key.setWeight(node.getWeight() + value);
+                        key.setParent(node);
+                        relaxed = true;
+                    }
+                }
+            }
+        }
+        return relaxed;
+    }
+
+    /**
+     * Calculates shortest path of a graph from the root node.
+     * For optimal results add the root as the first entry in the graph
+     *
+     * @param graph {@link WeightedGraph}
+     * @param root  root node
+     * @param <T>   Node label generic reference
+     * @throws StackOverflowError if the graph has a negative circular nodes meaning it can't be solved
+     */
+    public static <T> void calculateShortestPath(WeightedGraph<T> graph, WeightedGraph.Node<T> root)
+            throws StackOverflowError {
+        if(root.getWeight() == null){
+            root.setWeight(0.0);
+        }
+        for (int i = 1; i < graph.size(); i++) {
+            if(!relaxGraph(graph)){ // break early if the graph can no longer be relaxed
+                return;
+            }
+        }
+        if (relaxGraph(graph)) {
+            throw new StackOverflowError("Graph has a negative circular nodes");
+        }
+
+    }
+
+
+}
---
+++ src/test/java/com/cmuhatia/playground/graph/BellmanFordTest.java
@@ -0,0 +1,103 @@
+package com.cmuhatia.playground.graph;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import java.util.Map;
+import java.util.Set;
+
+import static org.junit.Assert.*;
+
+/**
+ * @author Cornelius M.
+ * @version 1.0.0, 09/06/2020
+ */
+public class BellmanFordTest {
+
+
+
+    @Test
+    public void calculateShortestPath() {
+        //Test first graph
+        WeightedGraph<String> graph = new WeightedGraph<>();
+
+        WeightedGraph.Node<String> a = new WeightedGraph.Node<>("A");
+        WeightedGraph.Node<String> b = new WeightedGraph.Node<>("B");
+        WeightedGraph.Node<String> c = new WeightedGraph.Node<>("C");
+        WeightedGraph.Node<String> d = new WeightedGraph.Node<>("D");
+        WeightedGraph.Node<String> e = new WeightedGraph.Node<>("E");
+
+        graph.addAll(Set.of(a, b, c, d, e));
+        assertEquals(5, graph.size());
+
+        a.setNeighbours(Map.of(b, -1.0, c, 4.0));
+        b.setNeighbours(Map.of(c, 3.0, d, 2.0, e, 2.0));
+        c.setNeighbours(Map.of());
+        d.setNeighbours(Map.of(c, 5.0, b, 1.0));
+        e.setNeighbours(Map.of(d, -3.0));
+
+        BellmanFord.calculateShortestPath(graph, a);
+
+        assertEquals(Double.valueOf(0.0), a.getWeight());
+        assertEquals(Double.valueOf(-1.0), b.getWeight());
+        assertEquals(Double.valueOf(2.0), c.getWeight());
+        assertEquals(Double.valueOf(-2.0), d.getWeight());
+        assertEquals(Double.valueOf(1.0), e.getWeight());
+
+
+    }
+
+    @Test
+    public void calculateShortestPath2(){
+        WeightedGraph<String> graph = new WeightedGraph<>();
+
+        WeightedGraph.Node<String> one = new WeightedGraph.Node<>("1");
+        WeightedGraph.Node<String> two = new WeightedGraph.Node<>("2");
+        WeightedGraph.Node<String> three = new WeightedGraph.Node<>("3");
+        WeightedGraph.Node<String> four = new WeightedGraph.Node<>("4");
+        WeightedGraph.Node<String> five = new WeightedGraph.Node<>("5");
+        WeightedGraph.Node<String> six = new WeightedGraph.Node<>("6");
+        WeightedGraph.Node<String> seven = new WeightedGraph.Node<>("7");
+
+        graph.addAll(Set.of(one, two, three, four, five, six, seven));
+        assertEquals(7, graph.size());
+
+        one.setNeighbours(Map.of(two, 6.0, three, 5.0, four, 5.0));
+        two.setNeighbours(Map.of(five, -1.0));
+        three.setNeighbours(Map.of(five, 1.0, two, -2.0));
+        four.setNeighbours(Map.of(three, -2.0, six, -1.0));
+        five.setNeighbours(Map.of(seven, 3.0));
+        six.setNeighbours(Map.of(seven, 3.0));
+
+        BellmanFord.calculateShortestPath(graph, one);
+
+        assertEquals(Double.valueOf(0.0), one.getWeight());
+        assertEquals(Double.valueOf(1.0), two.getWeight());
+        assertEquals(Double.valueOf(3.0), three.getWeight());
+        assertEquals(Double.valueOf(5.0), four.getWeight());
+        assertEquals(Double.valueOf(0.0), five.getWeight());
+        assertEquals(Double.valueOf(4.0), six.getWeight());
+        assertEquals(Double.valueOf(3.0), seven.getWeight());
+    }
+
+    @Test(expected = StackOverflowError.class)
+    public void calculateShortestPath3(){
+        WeightedGraph<String> graph = new WeightedGraph<>();
+
+        WeightedGraph.Node<String> one = new WeightedGraph.Node<>("1");
+        WeightedGraph.Node<String> two = new WeightedGraph.Node<>("2");
+        WeightedGraph.Node<String> three = new WeightedGraph.Node<>("3");
+        WeightedGraph.Node<String> four = new WeightedGraph.Node<>("4");
+
+        graph.addAll(Set.of(one, two, three, four));
+
+        one.setNeighbours(Map.of(two, 4.0, four, 5.0));
+        two.setNeighbours(Map.of(four, 5.0));
+        three.setNeighbours(Map.of(two, -10.0));
+        four.setNeighbours(Map.of(three, 3.0));
+
+        BellmanFord.calculateShortestPath(graph, one);
+
+
+    }
+}
