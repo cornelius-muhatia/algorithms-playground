0db8a3bb1639bbfd0df73430626607a1b5a22388
Cornelius Muhatia <muhatiacornelius@gmail.com>
2020-06-22 11:03:35 +0300

    Included solution for Faractional knapsack problem

--- src/main/java/com/cmuhatia/playground/knapsack/Knapsack.java
+++ src/main/java/com/cmuhatia/playground/knapsack/Knapsack.java
@@ -15,6 +15,7 @@
  */
 package com.cmuhatia.playground.knapsack;
 
+import java.util.Comparator;
 import java.util.List;
 import java.util.Map;
 
@@ -50,5 +51,34 @@ public class Knapsack {
             }
         }
         return table[items.size() - 1][capacity];
+    }
+
+    /**
+     * Gets maximum profit for factional Knapsack problem
+     *
+     * @param items  items with weight as the key and profit as the value
+     * @param capacity capacity of the knapsack
+     * @return maximum profit the knapsack can hold
+     */
+    public static double getMaxWeightFractional(List<Map.Entry<Integer, Integer>> items, int capacity){
+        items.sort((o1, o2) -> {
+            int item1Ratio = o1.getValue() / o1.getKey();
+            int item2Ratio = o2.getValue() / o2.getKey();
+            return (Integer.compare(item1Ratio, item2Ratio));
+        });
+        double filled = 0;
+        double maxWeight = 0.0;
+        for(int i = items.size() - 1; i > -1; i--){
+            if((filled + items.get(i).getKey()) <= capacity){
+                maxWeight += items.get(i).getValue();
+                filled += items.get(i).getKey();
+            } else if((capacity - filled) > 0){
+                double rem = capacity - filled;
+                maxWeight += (Double.valueOf(items.get(i).getValue()) / Double.valueOf(items.get(i).getKey())) * rem;
+            } else{
+                break;
+            }
+        }
+       return maxWeight;
     }
 }
--- src/test/java/com/cmuhatia/playground/knapsack/KnapsackTest.java
+++ src/test/java/com/cmuhatia/playground/knapsack/KnapsackTest.java
@@ -2,6 +2,7 @@ package com.cmuhatia.playground.knapsack;
 
 import org.junit.Test;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 
@@ -26,5 +27,23 @@ public class KnapsackTest {
         items = List.of(Map.entry(5, 10), Map.entry(4, 400), Map.entry(6, 30), Map.entry(4, 50));
         assertEquals(450, Knapsack.getMaxWeight01(items, 10));
 
+    }
+
+    @Test
+    public void getMaxWeightFractional() {
+        final double DELTA = 1e-15;
+        List<Map.Entry<Integer, Integer>> items = new ArrayList<>(List.of(Map.entry(10, 60), Map.entry(20, 100), Map.entry(30, 120)));
+
+        int capacity = 50;
+        assertEquals(240.0, Knapsack.getMaxWeightFractional(items, capacity), DELTA);
+
+        capacity = 50;
+        items = new ArrayList<>(List.of(Map.entry(10, 60), Map.entry(20, 100), Map.entry(30, 120)));
+        assertEquals(240, Knapsack.getMaxWeightFractional(items, capacity), DELTA);
+
+        capacity = 15;
+        items = new ArrayList<>(List.of(Map.entry(1, 5), Map.entry(3, 10), Map.entry(5, 15), Map.entry(4, 7),
+                Map.entry(1, 8), Map.entry(3, 9), Map.entry(2, 4)));
+        assertEquals(51, Knapsack.getMaxWeightFractional(items, capacity), DELTA);
     }
 }
