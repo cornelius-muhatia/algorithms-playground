2449e93b59b691be6622281dae7450791c3304e4
cornelius-muhatia <19651183+cornelius-muhatia@users.noreply.github.com>
2023-03-08 06:44:44 +0300

    Add merge k sorted lists

---
+++ leetcode-problems/src/main/java/com/cmuhatia/playground/leetcode/heap/MergeKSortedLists.java
@@ -0,0 +1,53 @@
+package com.cmuhatia.playground.leetcode.heap;
+
+import com.cmuhatia.playground.leetcode.dto.ListNode;
+import lombok.AccessLevel;
+import lombok.NoArgsConstructor;
+
+import java.util.Comparator;
+import java.util.PriorityQueue;
+
+/**
+ * <p>You are given an array of k linked-lists lists, each linked-list is sorted in ascending order.
+ * Merge all the linked-lists into one sorted linked-list and return it.</p>
+ *
+ * @see <a href="https://leetcode.com/problems/merge-k-sorted-lists/">LeetCode - Merge k Sorted Lists</a>
+ */
+@NoArgsConstructor(access = AccessLevel.PRIVATE)
+public class MergeKSortedLists {
+
+    /**
+     * Credits to: <a href="https://medium.com/javarevisited/java-algorithms-merge-k-sorted-lists-leetcode-84c99000bfd7">Rusian R.</a>
+     */
+    public static ListNode of(ListNode[] lists) {
+        if (lists == null || lists.length == 0) {
+            return null;
+        }
+
+        PriorityQueue<ListNode> heap = new PriorityQueue<>(
+                Comparator.comparingInt(ListNode::getVal)
+        );
+
+        for (ListNode node : lists) {
+            if (node != null) {
+                heap.add(node);
+            }
+        }
+
+        ListNode output = new ListNode(-1);
+        ListNode current = output;
+        while (!heap.isEmpty()) {
+            current.setNext(heap.remove());
+            ListNode next = current.getNext();
+
+            if (next.getNext() != null) {
+                heap.add(next.getNext());
+                next.setNext(null);
+            }
+
+            current = next;
+        }
+
+        return output.getNext();
+    }
+}
---
+++ leetcode-problems/src/test/java/com/cmuhatia/playground/leetcode/heap/MergeKSortedListsTest.java
@@ -0,0 +1,36 @@
+package com.cmuhatia.playground.leetcode.heap;
+
+import com.cmuhatia.playground.leetcode.dto.ListNode;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class MergeKSortedListsTest {
+
+    @Test
+    void of() {
+        ListNode first5 = new ListNode(5);
+        ListNode first4 = new ListNode(4, first5);
+        ListNode first = new ListNode(1, first4);
+
+        ListNode second4 = new ListNode(4);
+        ListNode second3 = new ListNode(3, second4);
+        ListNode second = new ListNode(1, second3);
+
+        ListNode third6 = new ListNode(6);
+        ListNode third = new ListNode(2, third6);
+
+        ListNode[] input = {first, second, third};
+
+        ListNode output6 = new ListNode(6);
+        ListNode output5 = new ListNode(5, output6);
+        ListNode output4 = new ListNode(4, output5);
+        ListNode output42 = new ListNode(4, output4);
+        ListNode output3 = new ListNode(3, output42);
+        ListNode output2 = new ListNode(2, output3);
+        ListNode output1 = new ListNode(1, output2);
+        ListNode output = new ListNode(1, output1);
+
+        assertEquals(output, MergeKSortedLists.of(input));
+    }
+}
